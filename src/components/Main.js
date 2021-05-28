dcfb8162129d75de16508322464c0961d7730ee2
Keola Morse <keolamorse@gmail.com>
2021-05-27 23:57:42 -0400

    change lazy load

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,42 +17,42 @@ const Main = () => (
         
         <section className="ind-list" data-aos="fade-in">
         
-            <div loading="lazy" className="right-one" data-aos="fade-right">
+            <div className="right-one" data-aos="fade-right">
                 <div className="right-img-row">
-                    <div className="ind-icon"><img src={Image.officeSup} alt="office"></img></div>
-                    <div className="right-img-container"><img id="office-img" src={Image.officeEquip} alt="office"></img></div>
+                    <div className="ind-icon"><img loading="lazy" src={Image.officeSup} alt="office"></img></div>
+                    <div className="right-img-container"><img loading="lazy" id="office-img" src={Image.officeEquip} alt="office"></img></div>
                 </div>
                 <div id="office-text" className="right-text">Office Product & Equipment Sales</div>
             </div>
             
-            <div loading="lazy" className="left-one" data-aos="fade-left">
+            <div className="left-one" data-aos="fade-left">
                 <div className="left-img-row">
                     <div className="left-img-container"><img loading="lazy" id="imt-img" src={Image.itSales} alt="IT"></img></div>
-                    <div className="ind-icon"><img src={Image.satellite} alt="IT"></img></div>
+                    <div className="ind-icon"><img loading="lazy" src={Image.satellite} alt="IT"></img></div>
                 </div>
                 <div id="it-text" className="left-text">Information Technology Sales</div>
             </div>
 
-            <div loading="lazy" className="right-one" data-aos="fade-right">
+            <div className="right-one" data-aos="fade-right">
                 <div className="right-img-row">
-                    <div className="ind-icon"><img src={Image.teleData} alt="telecom"></img></div>
-                    <div className="right-img-container"><img id="tele-img" src={Image.telecom} alt="telecom"></img></div>
+                    <div className="ind-icon"><img loading="lazy" src={Image.teleData} alt="telecom"></img></div>
+                    <div className="right-img-container"><img loading="lazy" id="tele-img" src={Image.telecom} alt="telecom"></img></div>
                 </div>
                 <div id="telecom" className="right-text">Telecommunications & Data Networking</div>
             </div>
 
-            <div loading="lazy" className="left-one" data-aos="fade-left">
+            <div className="left-one" data-aos="fade-left">
                 <div className="left-img-row">
-                    <div className="left-img-container"><img id="const-img" src={Image.construction} alt="construction"></img></div>
-                    <div className="ind-icon"><img src={Image.conArchEng} alt="construction"></img></div>
+                    <div className="left-img-container"><img loading="lazy" id="const-img" src={Image.construction} alt="construction"></img></div>
+                    <div className="ind-icon"><img loading="lazy" src={Image.conArchEng} alt="construction"></img></div>
                 </div>
                 <div id="construction" className="left-text">Construction, Architecture & Engineering</div>
             </div>
 
-            <div loading="lazy" className="right-one" data-aos="fade-right">
+            <div className="right-one" data-aos="fade-right">
                 <div className="right-img-row">
-                    <div className="ind-icon"><img src={Image.factory} alt="industry"></img></div>
-                    <div className="right-img-container"><img id="indust-img" src={Image.environmental} alt="industry"></img></div>
+                    <div className="ind-icon"><img loading="lazy" src={Image.factory} alt="industry"></img></div>
+                    <div className="right-img-container"><img loading="lazy" id="indust-img" src={Image.environmental} alt="industry"></img></div>
                 </div>
                 <div id="indust" className="right-text">Industrial & Environmental Services</div>
             </div>
@@ -64,13 +64,13 @@ const Main = () => (
         </div>
  
         
-        <section loading="lazy" className="ind-list" data-aos="fade-in">
+        <section  className="ind-list" data-aos="fade-in">
             
             <div className="left-one" data-aos="fade-left">
                
                 <div className="left-img-row">
-                    <div className="left-img-container"><img id="consumer-img" src={Image.retail} alt="retail"></img></div>
-                    <div className="ind-icon"><img src={Image.cashRegister} alt="retail"></img></div>
+                    <div className="left-img-container"><img loading="lazy" id="consumer-img" src={Image.retail} alt="retail"></img></div>
+                    <div className="ind-icon"><img loading="lazy" src={Image.cashRegister} alt="retail"></img></div>
                 </div>
                 
                 <div id="consumer" className="left-text">Consumer Goods & Retail</div>
@@ -78,32 +78,32 @@ const Main = () => (
 
             <div className="right-one" data-aos="fade-right">
                 <div className="right-img-row">
-                    <div className="ind-icon"><img src={Image.dining} alt="restaurant"></img></div>
-                    <div className="right-img-container"><img id="restaurant-img" src={Image.restaurant} alt="restaurant"></img></div>
+                    <div className="ind-icon"><img loading="lazy" src={Image.dining} alt="restaurant"></img></div>
+                    <div className="right-img-container"><img loading="lazy" id="restaurant-img" src={Image.restaurant} alt="restaurant"></img></div>
                 </div>
                 <div id="restaurant" className="right-text">Restaurant & Food Service Sales</div>
             </div>
 
             <div className="left-one" data-aos="fade-left">
                 <div className="left-img-row">
-                    <div className="left-img-container"><img id="pharma-img" src={Image.healthcare} alt="healthcare"></img></div>
-                    <div className="ind-icon"><img src={Image.stetho} alt="healthcare"></img></div>
+                    <div className="left-img-container"><img loading="lazy" id="pharma-img" src={Image.healthcare} alt="healthcare"></img></div>
+                    <div className="ind-icon"><img loading="lazy" src={Image.stetho} alt="healthcare"></img></div>
                 </div>
                 <div id="health" className="left-text">Healthcare Sales</div>
             </div>
 
             <div className="right-one" data-aos="fade-right">
                 <div className="right-img-row">
-                    <div className="ind-icon"><img src={Image.bank} alt="bank"></img></div>
-                    <div className="right-img-container"><img id="bank-img" src={Image.banking} alt="bank"></img></div>
+                    <div className="ind-icon"><img loading="lazy"src={Image.bank} alt="bank"></img></div>
+                    <div className="right-img-container"><img loading="lazy" id="bank-img" src={Image.banking} alt="bank"></img></div>
                 </div>
                 <div id="banking" className="right-text">Banking & Finance</div>
             </div>
 
             <div className="left-one" data-aos="fade-left">
                 <div className="left-img-row">
-                    <div className="ind-icon"><img src={Image.pharma} alt="pharma"></img></div>
-                    <div className="left-img-container"><img id="health-img" src={Image.medical} alt="pharma"></img></div>
+                    <div className="ind-icon"><img loading="lazy" src={Image.pharma} alt="pharma"></img></div>
+                    <div className="left-img-container"><img loading="lazy" id="health-img" src={Image.medical} alt="pharma"></img></div>
                 </div>
                 <div id="pharma" className="left-text">Pharmaceutical & Medical Device Sales</div>
             </div>
@@ -115,17 +115,18 @@ const Main = () => (
         <div id="video-section">
             <div id="video-frame" data-aos="fade-in" data-aos-delay="600">
                 
-                    <iframe id="video"
+                <LazyLoad>
+                    <iframe 
+                        id="video"
                         src="https://player.vimeo.com/video/39697431"
                         title="MRINetwork Impact Players"
-                        loading="lazy"
                         frameBorder="0"
                         webkitallowfullscreen="true"
                         mozallowfullscreen="true"
                         allowFullScreen={true}
                     >
                     </iframe>
-           
+                </LazyLoad>
             </div>
         </div>
        
