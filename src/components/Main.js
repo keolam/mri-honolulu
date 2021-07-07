49f027782989e7c197c9bf1908e61298917acfbd
Keola Morse <keolamorse@gmail.com>
2021-07-06 23:59:08 -0400

    lazy load

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,10 @@ import Image from '../Data/imageDir';
 import BottomNav from './BottomNav';
 import '../componentCSS/Landing.css'
 
-const Main = () => ( 
+const Main = () => (
 
     <section className="landing">
+
         <img src={Image.honolulu} id="diamond-head" alt="honolulu" data-aos="fade-in" data-aos-delay="300"></img>
         <section id="brand-title">
             <p id="brand-one" data-aos="fade-in" data-aos-delay="100">Your executive search solution for finding and recruiting
@@ -14,9 +15,9 @@ const Main = () => (
             </p>
             <p id="brand-two" data-aos="fade-in" data-aos-delay="200">Servicing businesses in the Hawaii & the Pacific Rim since 1988, specializing in: </p>
         </section>
-        
+
         <section className="ind-list" data-aos="fade-in">
-        
+
             <div className="right-one" data-aos="fade-right">
                 <div className="right-img-row">
                     <div className="ind-icon"><img loading="lazy" src={Image.officeSup} alt="office"></img></div>
@@ -24,7 +25,7 @@ const Main = () => (
                 </div>
                 <div id="office-text" className="right-text">Office Product & Equipment Sales</div>
             </div>
-            
+
             <div className="left-one" data-aos="fade-left">
                 <div className="left-img-row">
                     <div className="left-img-container"><img loading="lazy" id="imt-img" src={Image.itSales} alt="IT"></img></div>
@@ -57,22 +58,22 @@ const Main = () => (
                 <div id="indust" className="right-text">Industrial & Environmental Services</div>
             </div>
         </section>
-        
-    
+
+
         <div loading="lazy" id="midpage-graphic" data-aos="fade-in" data-aos-delay="10">
             <h2>We believe effective talent advisory and search firms can change lives and industries.</h2>
         </div>
- 
-        
-        <section  className="ind-list" data-aos="fade-in">
-            
+
+
+        <section className="ind-list" data-aos="fade-in">
+
             <div className="left-one" data-aos="fade-left">
-               
+
                 <div className="left-img-row">
                     <div className="left-img-container"><img loading="lazy" id="consumer-img" src={Image.retail} alt="retail"></img></div>
                     <div className="ind-icon"><img loading="lazy" src={Image.cashRegister} alt="retail"></img></div>
                 </div>
-                
+
                 <div id="consumer" className="left-text">Consumer Goods & Retail</div>
             </div>
 
@@ -94,7 +95,7 @@ const Main = () => (
 
             <div className="right-one" data-aos="fade-right">
                 <div className="right-img-row">
-                    <div className="ind-icon"><img loading="lazy"src={Image.bank} alt="bank"></img></div>
+                    <div className="ind-icon"><img loading="lazy" src={Image.bank} alt="bank"></img></div>
                     <div className="right-img-container"><img loading="lazy" id="bank-img" src={Image.banking} alt="bank"></img></div>
                 </div>
                 <div id="banking" className="right-text">Banking & Finance</div>
@@ -107,16 +108,16 @@ const Main = () => (
                 </div>
                 <div id="pharma" className="left-text">Pharmaceutical & Medical Device Sales</div>
             </div>
-      
+
         </section>
-        
-        
+
+
 
         <div id="video-section">
             <div id="video-frame" data-aos="fade-in" data-aos-delay="600">
-                
-                <LazyLoad>
-                    <iframe 
+
+                <LazyLoad height={250} once>
+                    <iframe
                         id="video"
                         src="https://player.vimeo.com/video/39697431"
                         title="MRINetwork Impact Players"
@@ -129,7 +130,7 @@ const Main = () => (
                 </LazyLoad>
             </div>
         </div>
-       
+
         <BottomNav name="Main" />
     </section>
 )
